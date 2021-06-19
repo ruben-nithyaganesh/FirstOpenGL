d6959091b7200b2a7d2430cda468ec2303837259
ruben-nithyaganesh <rubennithyaganesh@gmail.com>
2021-06-19 13:48:52 +1200

    ModernOpenGL

--- FirstOpenGL/src/Source.cpp
+++ FirstOpenGL/src/Source.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -19,18 +21,33 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK) {
+        std::cout << "glewInit() failed" << std::endl;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+    
+
+    float positions[] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    //'buffer' is the ID of the generated buffer
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    //bindBuffer specifies buffer to be drawn
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_POLYGON);
-        glVertex2f(-0.5f, 0.5f);
-        glVertex2f(0.0f,   0.5f);
-        glVertex2f(0.5f,  -0.5f);
-        glVertex2f(-0.6, -0.6);
-        glEnd();      
+        glDrawArrays(GL_TRIANGLES, 0, 3);
                       
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
